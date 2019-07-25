0ccfd38d64599d605a4484036034f751a310ad5d
Iqbal <iqbalatmadja@gmail.com>
2019-07-25 09:56:32 +0700

    fix layout dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,56 +14,23 @@
         @include('dashboard.partials.sidenav')
         <div class="main-panel">
             <div class="content-wrapper">
-                <div class="row">
-                    <div class="col-md-12 grid-margin">
-                        <div class="d-flex justify-content-between flex-wrap">
-                            <div class="d-flex align-items-end flex-wrap">
-                                <div class="mr-md-3 mr-xl-5">
-                                    <h2>Welcome back,</h2>
-                                    <p class="mb-md-0">Your analytics dashboard template.</p>
-                                </div>
-                                <div class="d-flex">
-                                    <i class="mdi mdi-home text-muted hover-cursor"></i>
-                                    <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
-                                    <p class="text-primary mb-0 hover-cursor">Analytics</p>
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-between align-items-end flex-wrap">
-                                <button type="button" class="btn btn-light bg-white btn-icon mr-3 d-none d-md-block ">
-                                  <i class="mdi mdi-download text-muted"></i>
-                                </button>
-                                <button type="button" class="btn btn-light bg-white btn-icon mr-3 mt-2 mt-xl-0">
-                                  <i class="mdi mdi-clock-outline text-muted"></i>
-                                </button>
-                                <button type="button" class="btn btn-light bg-white btn-icon mr-3 mt-2 mt-xl-0">
-                                  <i class="mdi mdi-plus text-muted"></i>
-                                </button>
-                                <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
-                            </div>
-                        </div>
+            	<div class="row">
+				    <div class="col-md-12 grid-margin">		
+		        		@include('dashboard.partials.dashboardIndexHeader')
                     </div>
                 </div>
                 <div class="row" id="proBanner">
-                    <div class="col-md-12 grid-margin">
-                        <div class="card bg-gradient-primary border-0">
-                            <div class="card-body py-3 px-4 d-flex align-items-center justify-content-between flex-wrap">
-                                <p class="mb-0 text-white font-weight-medium">Like what you see? Checkout our premium version for more.</p>
-                                <div class="d-flex">
-                                    <a href="https://github.com/Urbanui/MajesticAdmin-Free-Bootstrap-Admin-Template" target="_blank" class="btn btn-outline-light mr-2">Download free version</a>
-                                    <a href="http://www.urbanui.com/majestic-admin-pro/template/" target="_blank" class="btn btn-outline-light mr-2 bg-gradient-danger border-0">Upgrade to Pro</a>
-                                    <button id="bannerClose" class="btn border-0 p-0">
-                                        <i class="mdi mdi-close text-white"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+				    <div class="col-md-12 grid-margin">	
+						@include('dashboard.partials.dashboardBanner')
                     </div>
                 </div>
+                
                 <div class="row">
                     <div class="col-md-12 grid-margin stretch-card">
                         @include('dashboard.partials.statReview')
                     </div>
                 </div>
+                
                 <div class="row">
                     <div class="col-md-7 grid-margin stretch-card">
                         <div class="card">
---
+++ resources/views/dashboard/partials/dashboardBanner.blade.php
@@ -0,0 +1,12 @@
+<div class="card bg-gradient-primary border-0">
+    <div class="card-body py-3 px-4 d-flex align-items-center justify-content-between flex-wrap">
+        <p class="mb-0 text-white font-weight-medium">Like what you see? Checkout our premium version for more.</p>
+        <div class="d-flex">
+            <a href="https://github.com/Urbanui/MajesticAdmin-Free-Bootstrap-Admin-Template" target="_blank" class="btn btn-outline-light mr-2">Download free version</a>
+            <a href="http://www.urbanui.com/majestic-admin-pro/template/" target="_blank" class="btn btn-outline-light mr-2 bg-gradient-danger border-0">Upgrade to Pro</a>
+            <button id="bannerClose" class="btn border-0 p-0">
+                <i class="mdi mdi-close text-white"></i>
+            </button>
+        </div>
+    </div>
+</div>
