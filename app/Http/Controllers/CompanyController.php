7bfb918541c4fcba085cd3e1f2d34a20cee48a25
Iqbal <iqbalatmadja@gmail.com>
2019-07-31 11:40:42 +0700

    playing with model

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Company;
+//use App\Company;
 
 class CompanyController extends Controller
 {
     //
     public function list(Request $request)
     {
-        $companies = Company::all();
+        //$companies = Company::all(); # will need "use App\Company;" above
+        // or
+        $companies = \App\Company::all(); # dont need "use App\Company;" above
         $data = ['companies'=>$companies];
         return view('company.list',$data);
     }
