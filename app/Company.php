2899d9679fbf7eb47d3ad794e88f6d4ca6a84eb2
Iqbal <iqbalatmadja@gmail.com>
2019-07-31 10:41:05 +0700

    created company model

--- app/Company.php
+++ app/Company.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     //
+    protected $table = "companies"; # default to companies (plural of model name
+    protected $primaryKey = "id"; # default to id
+    public $incrementing = true; # default to true
+    //protected $keyType = 'string'; # primary key type, default to integer
+    public $timestamps = true; # using timestamps (created_at,updated_at)default to true
+    const CREATED_AT = 'creation_at'; # default to created_at
+    const UPDATED_AT = 'updated_at'; # default to updated_at
+    /*
+    protected $attributes = [
+        'delayed' => false,
+    ];
+    */ # defining default attribute values
+    
 }
