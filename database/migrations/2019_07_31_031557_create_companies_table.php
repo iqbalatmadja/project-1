573cf78ec6545d95dc5f58a1fc4cc252505ffb49
Iqbal <iqbalatmadja@gmail.com>
2019-07-31 10:29:13 +0700

    created table companies

---
+++ database/migrations/2019_07_31_031557_create_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',64)->nullable(false)->default('Untitled');
+            $table->tinyInteger('is_active',0,1)->nullable(false)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
