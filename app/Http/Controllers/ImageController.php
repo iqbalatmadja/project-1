e9bec562dd9ff0621ddac2d5bbca3e9acac86756
Iqbal <iqbalatmadja@gmail.com>
2019-08-08 10:48:36 +0700

    added preview before submit upload

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -49,39 +49,5 @@ class ImageController extends Controller
         
     }
 
-    /*
-    public function index(Request $request)
-    {
-        $data = [];
-        return view('image.index',$data);
-    }
-    */
     
-    /*
-    public function save(Request $request)
-    {
-        $imageFile = $request->file('file');
-        $imageName = $imageFile->getClientOriginalName();
-        $imageFile->move(public_path('uploads/images'),$imageName);
-        
-        $image = new Image;
-        $image->filename = $imageName;
-        $image->user_id = Auth()->user()->id;
-        $image->save();
-        return response()->json(['success'=>$imageName]);
-    }
-    */
-    
-    /*
-    public function delete(Request $request)
-    {
-        $filename =  $request->get('filename');
-        Image::where('filename',$filename)->delete();
-        $path=public_path().'/uploads/images/'.$filename;
-        if (file_exists($path)) {
-            unlink($path);
-        }
-        return $filename;
-    }
-    */
 }
